25a956f2b741f39eae9b3ed414f329d87ebea3cb
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2024-11-10 18:33:39 +0200

    Added dbContext

---
+++ HackathonAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HackathonAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+    }
+}
