b4c1f479d71cde7c3b6d2105f3fb639845817b83
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2024-11-10 18:38:38 +0200

    Added Service extensions

---
+++ HackathonAPI/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using HackathonAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackathonAPI.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opts =>
+            {
+                opts.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opts =>
+            {
+                opts.AddPolicy("AllowAny", opts => opts.AllowAnyHeader().AllowAnyMethod()
+                 .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+            });
+
+            return services;
+        }
+    }
+}
---
+++ HackathonAPI/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace HackathonAPI.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opts =>
+            {
+                var tokenKey = configuration["tokenKey"] ?? throw new Exception("Token key not found ");
+                opts.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+    }
+}
